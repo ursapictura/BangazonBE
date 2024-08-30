3cbe96239ed0ff7d108a93d864c2deb02836e764
ursapictura <hsmithfreelance@gmail.com>
2024-08-29 21:22:01 -0500

    Firebasekey changed to Uid. Database updated

---
+++ Migrations/20240830021715_ChangeFirebaseKeyToUid.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BangazonBE.Migrations
+{
+    public partial class ChangeFirebaseKeyToUid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirebaseKey",
+                table: "Users",
+                newName: "Uid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PaymentTypeId",
+                table: "Orders",
+                column: "PaymentTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_PaymentTypes_PaymentTypeId",
+                table: "Orders",
+                column: "PaymentTypeId",
+                principalTable: "PaymentTypes",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_PaymentTypes_PaymentTypeId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_PaymentTypeId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "Uid",
+                table: "Users",
+                newName: "FirebaseKey");
+        }
+    }
+}
